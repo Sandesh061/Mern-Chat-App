4ea1c6dc86a59ade1599c4fe4f1dff9e2fca70c9
Sandesh Pansare <87933775+Sandesh061@users.noreply.github.com>
2024-01-18 14:53:27 +0530

    Update connection.js

--- server/connection.js
+++ server/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-mongoose.connect(`mongodb+srv://admin:YuvrajPardeshi@cluster0.ppm5ju4.mongodb.net/chat-app?retryWrites=true&w=majority`, ()=> {
+mongoose.connect(`mongodb+srv://Mern_chat_app:Sandesh2001@cluster0.j28fpbd.mongodb.net/chat-app?retryWrites=true&w=majority`, ()=> {
   console.log('connected to mongodb')
 })
